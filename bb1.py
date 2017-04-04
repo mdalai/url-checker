941c6d99a838a7f9ca01d302c52090633362cfbc
swidc <swidc@cuw.edu>
2017-04-04 14:38:34 -0500

    update codes

--- bb1.py
+++ bb1.py
@@ -6,36 +6,13 @@ from bs4 import BeautifulSoup
 from PyQt5 import QtWidgets
 
 
-def get_next_target(page):
-    a_start_pos = page.find("<a href=")
-    if a_start_pos == -1:
-        return None,None, 0
-    id_start_pos = page.find("Course&id=", a_start_pos)
-    id_end_pos = page.find("&", id_start_pos+10)
-    courseid = page[id_start_pos+10:id_end_pos]
-
-    a_end_pos = page.find(">", a_start_pos+1)
-    title_start_pos = a_end_pos + 1
-    title_end_pos = page.find("</a>", title_start_pos+1)
-    coursename = page[title_start_pos:title_end_pos]
-    return coursename,courseid, title_end_pos
-
-def get_all_target(page):
-    p =[]
-    while True:
-        coursename,courseid,endpos = get_next_target(page)
-        if coursename:
-            p.append([coursename,courseid])
-            page = page[endpos:]
-        else:
-            break
-    return p
-
-
-class BB(object):
+class Blackboard(object):
     def __init__(self, session):
         self.session = session
+        # keep all urls in following global list variable
+        self.urlList = []
 
+    ## -----  1. get Course list --------------------------------------------------
     def getCourses(self):
         # https://concordia.blackboard.com/webapps/portal/execute/tabs/tabAction?action=refreshAjaxModule&modId=_22_1&tabId=_2_1&tab_tab_group_id=_2_1
         payload = {'action': 'refreshAjaxModule', 'modId': '_22_1', 'tabId': '_2_1', 'tab_tab_group_id':'_2_1'}
@@ -46,13 +23,39 @@ class BB(object):
         text_end_pos = text.find("</ul")
         text = text[text_start_pos:text_end_pos]
 
-        return get_all_target(text)
-        
-        #courses = get_all_target(text)
-        #with open("courseid.txt","w") as f:
-        #    f.writelines('%s\n' % '\t'.join(items) for items in courses)
+        return self.get_all_target(text)
 
-    def getContents(self, courseid):
+    def get_all_target(self,page):
+        p =[]
+        while True:
+            coursename,courseid,endpos = self.get_next_target(page)
+            if coursename:
+                p.append([coursename,courseid])
+                page = page[endpos:]
+            else:
+                break
+        return p
+
+    def get_next_target(self,page):
+        a_start_pos = page.find("<a href=")
+        if a_start_pos == -1:
+            return None,None,0
+        id_start_pos = page.find("Course&id=", a_start_pos)
+        id_end_pos = page.find("&", id_start_pos+10)
+        courseid = page[id_start_pos+10:id_end_pos]
+
+        a_end_pos = page.find(">", a_start_pos+1)
+        title_start_pos = a_end_pos + 1
+        title_end_pos = page.find("</a>", title_start_pos+1)
+        coursename = page[title_start_pos:title_end_pos]
+        return coursename,courseid, title_end_pos
+            
+        
+    ## -----  2. get URL list from a course --------------------------------------
+    def getURLs(self, courseid):
+        # --- clean the variable every time ----
+        self.urlList = []
+        
         payload_temp = {'course_id': courseid}
         r_temp = self.session.get("https://concordia.blackboard.com/webapps/blackboard/content/courseMenu.jsp",
                              params = payload_temp)
@@ -64,36 +67,37 @@ class BB(object):
         cnt_id = text_temp[cnt_start_pos+11:cnt_end_pos]
         #print cnt_id
         payload = {'course_id': courseid, 'content_id': cnt_id, 'mode':'reset' }
-        r = self.session.get("https://concordia.blackboard.com/webapps/blackboard/content/listContentEditable.jsp",
+        r = self.session.get("https://concordia.blackboard.com/webapps/blackboard/content/listContent.jsp",
                              params = payload)
         soup = BeautifulSoup(r.text,"html.parser")
-        links =[]
+
+        if soup.find(id="content_listContainer") == None:
+            return []
+
         for li in soup.find(id="content_listContainer").findAll("li", recursive=False):
             # if it is folder
             if li.find("img")['src'][-13:] == 'folder_on.gif' and li.find("div").find("h3").find("a"):
                 wk = li.find("div").find("h3").find("a").find("span").contents[0] 
                 # get the Content ID from URL
                 contentid = self._getContentIdFromUrl(li.find("div").find("h3").find("a")['href'])
-                link = self.openFolder(courseid, contentid, wk)
-                if link:
-                    links.append(link)
+                # get links from the folder
+                self.openFolder(courseid, contentid, wk)
 
             # else if it is File, Test, Discussion,Dropbox ...
-        else:
-            name = u'\\'.join([unicode("ROOT"), 
-            unicode(li.find("div").find("h3").find("span",{"style":"color:#000000;"}).contents[0])])
-            #print name
-            page = str(li.find("div",{"class":"vtbegenerated"})) # must convert Soup to string
-            links2= self.get_all_links(page)
-            if links2:
-                links.append([name, links2])
+            else:
+                name = u'\\'.join([unicode("ROOT"), 
+                unicode(li.find("div").find("h3").find("span",{"style":"color:#000000;"}).contents[0])])
+                #print name
+                page = str(li.find("div",{"class":"vtbegenerated"})) # must convert Soup to string
+                links2= self.get_all_links(page)
+                if links2:
+                    self.urlList.append([name, links2])
 
-        return links
+        return self.urlList
 
-    # this function has not used
     def _getContentIdFromUrl(self, url):
         link = urlparse.urlparse(url)
-        if link.path == "/webapps/blackboard/content/listContentEditable.jsp":
+        if link.path == "/webapps/blackboard/content/listContent.jsp":
             q = urlparse.parse_qs(link.query)
             if q.has_key('content_id'):
                 return q['content_id'][0]
@@ -111,29 +115,31 @@ class BB(object):
     def openFolder(self,courseid, contentid, title):
         try:
             payload = {'course_id': courseid, 'content_id': contentid, 'mode':'reset'}
-            r = self.session.get("https://concordia.blackboard.com/webapps/blackboard/content/listContentEditable.jsp",
+            r = self.session.get("https://concordia.blackboard.com/webapps/blackboard/content/listContent.jsp",
                                 params = payload)
             soup = BeautifulSoup(r.text,"html.parser")
 
-            linksList = []
+            #linksList = []
             # if it is empty Folder return none
             if soup.find(id="content_listContainer") == None:
-                return None
+                #self.urlList.append(['Empty Folder',None])
+                return            
 
             for li in soup.find(id="content_listContainer").findAll("li", recursive=False):
                 # if it is folder
                 if li.find("img")['src'][-13:] == 'folder_on.gif' and li.find("div").find("h3").find("a"):
                     #--- if there are links under folder ----
-                    #if li.find("div",{"class":"vtbegenerated"}).find("a"):
+                    wk = u'\\'.join([unicode(title), unicode(li.find("div").find("h3").find("a").find("span").contents[0])]) 
                     if li.find("div",{"class":"vtbegenerated"}):
                         page = str(li.find("div",{"class":"vtbegenerated"})) # must convert Soup to string
                         links= self.get_all_links(page)
                         if links:
-                            linksList.append([name, links])
-                    wk = u'\\'.join([unicode(title), unicode(li.find("div").find("h3").find("a").find("span").contents[0])])                                
+                            self.urlList.append([wk, links])
+                               
                     contentid = self._getContentIdFromUrl(li.find("div").find("h3").find("a")['href'])
-                    linksList.append(self.openFolder(courseid, contentid, wk))
-                    #return self.openFolder(courseid, contentid, wk)
+                    # keep digging into the folder
+                    self.openFolder(courseid, contentid, wk)
+
 
                 # if it is File
                 elif li.find("img")['src'][-15:] == 'document_on.gif':
@@ -143,7 +149,7 @@ class BB(object):
                     page = str(li.find("div",{"class":"vtbegenerated"})) # must convert Soup to string
                     links= self.get_all_links(page)
                     if links:
-                        linksList.append([name, links])
+                        self.urlList.append([name, links])
 
                 # else if it is Test, Discussion, Dropbox ...
                 else:
@@ -153,9 +159,9 @@ class BB(object):
                     page = str(li.find("div",{"class":"vtbegenerated"})) # must convert Soup to string
                     links= self.get_all_links(page)
                     if links:
-                        linksList.append([name, links])
-            if linksList:
-                return linksList
+                        self.urlList.append([name, links])
+            #if linksList:
+            #    return linksList
                 #print linksList
 
         except Exception as e:
@@ -176,7 +182,7 @@ class BB(object):
 
     def get_next_link(self, page):
         a_link = page.find('<a ')
-        start_link = page.find(' href="http', a_link+1)
+        start_link = page.find(' href="', a_link+1) #http
         if start_link ==  -1:
             return None, 0
         start_quote = page.find('"', start_link)
@@ -184,6 +190,7 @@ class BB(object):
         url = page[start_quote + 1: end_quote]
         return url, end_quote
 
+    ## -----  3. check URL list --------------------------------------
     def urlStatus_code(self, url):
         #-- flag = 0: No problem, 1: problem, 2: further check
         try:
@@ -216,6 +223,44 @@ class BB(object):
                 return response.status_code,None,2
         except Exception as ex:
             return 777,ex,2
+
+
+
+    #-- Check YOUTUBE URLs 
+    def youtubeStatus(self,url):
+        
+        if url.find("v=") != -1:
+            start_pos = url.find("v=") + 2
+    	    end_pos = url.find("&", start_pos)
+    	    if end_pos == -1:
+    		end_pos = url.find("#", start_pos)
+    		if end_pos == -1:
+    		    youtube_id = url[start_pos:]
+    		else:
+    		    youtube_id = url[start_pos:end_pos]
+    	    else:
+    		youtube_id = url[start_pos:end_pos]
+    	elif url.find(".be/") != -1:
+    	    start_pos = url.find(".be/") + 4
+    	    end_pos = url.find("?", start_pos)
+            if end_pos == -1:
+                youtube_id = url[start_pos:]
+            else:
+                youtube_id = url[start_pos:end_pos]
+        else:
+            return "Youtube Channel"
+
+    	#print youtube_id
+    	try:
+    	    urlAPI = "https://www.googleapis.com/youtube/v3/videos?part=status&id=%s&key=AIzaSyAOBqTeqHaI1JTXzJNfQOzZZ-rMGmALHBw"%youtube_id
+    	    response = urllib.urlopen(urlAPI)
+    	    result = json.loads(response.read())
+    	    if result["items"] == []:
+    		return "UNAVAILABLE"
+    	    else:
+    		return result['items'][0]['status']['uploadStatus']
+    	except Exception, e:
+    	    return "EXCEPTION"  
     
 
 
--- pyqt_bb.py
+++ pyqt_bb.py
@@ -1,7 +1,6 @@
 import sys
-#from PyQt5 import QtCore, QtGui, uic 
 from PyQt5 import QtWidgets, uic, QtCore, QtGui
-from bb1 import BB
+from bb1 import Blackboard
 import requests
 import pandas as pd
 import numpy as np
@@ -11,16 +10,6 @@ import webbrowser
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
-
-def peelNestedList(link):
-  if type(link[0]) == type([]) and len(link) > 1:
-    for p in link:
-      peelNestedList(p)
-  else:
-      if type(link[0]) != type([]):
-        nameUrls.append([link[0],link[1]])
-      else:
-        peelNestedList(link[0])
 
 qtCreatorFile1 = "bb_urls.ui" # Enter file here.
 Ui_urlWindow , QtBaseClass = uic.loadUiType(qtCreatorFile1)
@@ -50,10 +39,13 @@ class MyApp(QtWidgets.QMainWindow, Ui_Window):
 
         #-- create session --#
         self.session = requests.Session()
+        # initialize Blackboard class
+        self.bb = Blackboard(self.session)
 
+        # Login 
         self.pushButton_getCourses.clicked.connect(self.login)
-        self.pushButton_checkURLs.clicked.connect(self.checkURLs)    	
-
+        # Check URL
+        self.pushButton_checkURLs.clicked.connect(self.checkURLs)	
 
     def logout(self):
     	#print self.session.cookies.get_dict()
@@ -63,7 +55,6 @@ class MyApp(QtWidgets.QMainWindow, Ui_Window):
     def login(self):
     	user = self.lineEdit_username.text()
     	pwd = self.lineEdit_pass.text()
-    	#print user,pwd
     	#-- Start Wait Curser -----------------------------------------------------------------
     	QtWidgets.QApplication.setOverrideCursor(QtGui.QCursor(QtCore.Qt.WaitCursor))
     	
@@ -80,11 +71,14 @@ class MyApp(QtWidgets.QMainWindow, Ui_Window):
         try:
         	# make session for visiting blackboard site
         	r = self.session.post("https://concordia.blackboard.com/webapps/login/", data=payload)
-        	courses =[]        	
-        	bb = BB(self.session)
-        	courses = bb.getCourses()
+        	# Create a list to store all courses
+        	courses =[]
+        	# recall getCourses method from Blackboard Class
+        	courses = self.bb.getCourses()
       		if courses != []:
-      			print "success login!"
+      			print "Login Success!"
+
+      		# Use TableWidget to show course list
        		self.tableWidget_courses.setColumnCount(2)
        		self.tableWidget_courses.setRowCount(len(courses))
        		self.tableWidget_courses.setHorizontalHeaderLabels(['Courses Name', 'Courses ID'])
@@ -93,62 +87,16 @@ class MyApp(QtWidgets.QMainWindow, Ui_Window):
        			self.tableWidget_courses.setItem(i, 0, QtWidgets.QTableWidgetItem(p[0]))
        			self.tableWidget_courses.setItem(i, 1, QtWidgets.QTableWidgetItem(p[1]))
        			i = i+1
-
         	self.tableWidget_courses.resizeColumnsToContents()     	        	
 
         except Exception as e:
         	print e
-        	QtWidgets.QMessageBox.about(self, "My message box", "Server Exception! please try again!!" )
+        	QtWidgets.QMessageBox.about(self, "Warning message box", "Server Exception! please try again!!" )
 
         #-- End Wait Curser -----------------------------------------------------------------
         QtWidgets.QApplication.restoreOverrideCursor() 
 
-    def checkURLs(self):
-    	#-- inner function for solving nested list
-    	def peelNestedList(link):
-            if type(link[0]) == type([]) and len(link) > 1:
-                for p in link:
-                    peelNestedList(p)
-            else:
-    			if type(link[0]) != type([]):
-    				nameUrls.append([link[0],link[1]])
-    			else:
-    				peelNestedList(link[0])
-    	#-- Check YOUTUBE URLs 
-    	def youtubeStatus(url):
-    		if url.find("v=") != -1:
-    			start_pos = url.find("v=") + 2
-    			end_pos = url.find("&", start_pos)
-    			if end_pos == -1:
-    				end_pos = url.find("#", start_pos)
-    				if end_pos == -1:
-    					youtube_id = url[start_pos:]
-    				else:
-    					youtube_id = url[start_pos:end_pos]
-    			else:
-    				youtube_id = url[start_pos:end_pos]
-    		elif url.find(".be/") != -1:
-    			start_pos = url.find(".be/") + 4
-    			end_pos = url.find("?", start_pos)
-                        if end_pos == -1:
-                            youtube_id = url[start_pos:]
-                        else:
-                            youtube_id = url[start_pos:end_pos]
-                else:
-                    return "Youtube Channel"
-
-    		#print youtube_id
-    		try:
-    			urlAPI = "https://www.googleapis.com/youtube/v3/videos?part=status&id=%s&key=AIzaSyAOBqTeqHaI1JTXzJNfQOzZZ-rMGmALHBw"%youtube_id
-    			response = urllib.urlopen(urlAPI)
-    			result = json.loads(response.read())
-    			if result["items"] == []:
-    				return "UNAVAILABLE"
-    			else:
-    				return result['items'][0]['status']['uploadStatus']
-    		except Exception, e:
-    			return "EXCEPTION"    	
-    	
+    def checkURLs(self):    	
     	row = self.tableWidget_courses.currentRow()
         #print row
         if row == -1:
@@ -164,8 +112,7 @@ class MyApp(QtWidgets.QMainWindow, Ui_Window):
     	QtWidgets.QApplication.setOverrideCursor(QtGui.QCursor(QtCore.Qt.WaitCursor))
     	
     	#print course_id
-    	bb = BB(self.session)
-    	links = bb.getContents(course_id)
+    	links = self.bb.getURLs(course_id)
     	#---------- Test Start ---------------------------------
     	#with open('urlTest.txt', 'rb') as f:
     	#	links = pickle.load(f)
@@ -180,44 +127,41 @@ class MyApp(QtWidgets.QMainWindow, Ui_Window):
     	#---------- Test End ---------------------------------
         #print links
         if links == []:
-            QtWidgets.QMessageBox.about(self, "My message box", "Returns no links. Please close the whole program and restry it." )
+            QtWidgets.QMessageBox.about(self, "My message box", "Returns no link. Please check the CONTENT in the Blackboard!!" )
             return
         #with open('linksTest.txt', 'wb') as f:
         #    pickle.dump(links, f)
 
-    	nameUrls = []
-    	for link in links:
-            if link == [None]:
-                continue
-            peelNestedList(link)
-
-
+        # ----- Check the URL responses and keep data into Panda DF ---------------------------------
     	df = pd.DataFrame(columns=('Location','URL','Status_code','Remark','flag'))
-    	for urls in nameUrls:
-    		for url in urls[1]:
-    			if url.find("youtube.com") == -1 and url.find("youtu.be/") == -1:
-                                #print url                            
-    				status_code, comment, flag = bb.urlStatus_code(url)
-    				df = pd.DataFrame(np.array([[urls[0], url, status_code, comment, flag]]),
-            			columns=['Location','URL','Status_code','Remark','flag']).append(df, ignore_index=True)
-    			else:
-    				flag = 1
-    				r = requests.head(url, headers={'User-Agent': 'user_agent',})
-    				if r.status_code != 404:
-    					status = youtubeStatus(url)
-    					if status == "processed" or status == "Youtube Channel":
-    						flag = 0
-    					elif status == "UNAVAILABLE" or status == "EXCEPTION":
-    						flag = 2
-    					else:
-    						flag = 1
+    	for urls in links:
+    	  for url in urls[1]:
+            if url.find("youtube.com") == -1 and url.find("youtu.be/") == -1:          
+              #print url                            
+    	      status_code, comment, flag = self.bb.urlStatus_code(url)
+    	      df = pd.DataFrame(np.array([[urls[0], url, status_code, comment, flag]]),
+              columns=['Location','URL','Status_code','Remark','flag']).append(df, ignore_index=True)
+    	    else:
+    	      flag = 1
+    	      r = requests.head(url, headers={'User-Agent': 'user_agent',})
+    	      if r.status_code != 404:
+    		status = self.bb.youtubeStatus(url)
+    		if status == "processed" or status == "Youtube Channel":
+    		  flag = 0
+    		elif status == "UNAVAILABLE" or status == "EXCEPTION":
+    		  flag = 2
+    		else:
+    		  flag = 1
 
-    				else:
-    					status = "YOUTUBE Not Found"
-    				df = pd.DataFrame(np.array([[urls[0], url, r.status_code, status, flag]]),
-            			columns=['Location','URL','Status_code','Remark','flag']).append(df, ignore_index=True)
+    	      else:
+    		  status = "YOUTUBE Not Found"
+    	      df = pd.DataFrame(np.array([[urls[0], url, r.status_code, status, flag]]),
+              columns=['Location','URL','Status_code','Remark','flag']).append(df, ignore_index=True)
 
+    	#---- Reverse DF order
+    	df = df.iloc[::-1]
 
+    	#----- Show the data of DF into a TableWidget ---------------
     	url_window.tableWidget_urls.setColumnCount(len(df.columns))
     	url_window.tableWidget_urls.setRowCount(len(df.index))
     	url_window.tableWidget_urls.setHorizontalHeaderLabels(['Location','URL','Status_code','Remark','flag'])
@@ -242,8 +186,6 @@ class MyApp(QtWidgets.QMainWindow, Ui_Window):
         # if Pandas Dataframe is not empty, save to a csv file
         if not df.empty:
           df.to_csv(course_id +'.csv', encoding='utf-8')
-
-
 
 
 if __name__ == "__main__":
